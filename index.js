01fb6d654280bc6b70556f7b2e1d2fad7f5d2dfc
Fábio Theophilo <fabiotheophilo@gmail.com>
2020-01-11 18:12:34 +0000

    Finishes the scraping

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const puppeteer = require('puppeteer');
+const jsdom = require("jsdom")
+const puppeteer = require('puppeteer')
+
+const { JSDOM } = jsdom
 
 const fetchHumbleBundle = async() => {
   const browser = await puppeteer.launch();
@@ -13,4 +16,27 @@ const fetchHumbleBundle = async() => {
   return result
 }
 
-fetchHumbleBundle().then(console.log)
+const parseCard = source => {
+  const { document } = (new JSDOM(source)).window;
+
+  return {
+    title: document.querySelector('.name').textContent,
+    timeLeft: document.querySelector('.timer-field').textContent,
+    description: document.querySelector('.detailed-marketing-blurb').textContent,
+    highlights: [...document.querySelectorAll('.highlight')].map(h => h.textContent),
+    image: document.querySelector('img').attributes['data-src'].textContent
+  }
+}
+
+const parsePage = page => {
+  const { document } = (new JSDOM(page)).window;
+
+  const humbleCards = [...document.querySelectorAll('a.simple-tile-view')]
+    .map(card => parseCard(card.innerHTML))
+
+  return humbleCards
+}
+
+fetchHumbleBundle()
+  .then(parsePage)
+  .then(console.log)
